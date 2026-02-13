f91a109c783a31080aa67651d9fffc9054ee5b98
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-02-13 17:21:08 +0100

    add functionality to display categories with dummy data

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import "../startScreen.css";
 
 export const StartScreen = (props) => {
+  const [categories, setCategories] = useState("");
+
+  //Replace by API data
+  const categoryList = ["sport", "animals", "food", "geography"];
+
+  const chooseCategory = categoryList.map((category) => {
+    return (
+      <option key={category} value={category}>
+        {category}
+      </option>
+    );
+  });
+
+  //Update the state variable on any change
+  //
+  function handleCategories(e) {
+    setCategories(e.target.value);
+  }
+
+  // RETURN
   return (
     <div className="start-screen">
       <header>
@@ -10,11 +31,14 @@ export const StartScreen = (props) => {
       <main>
         <form>
           <label htmlFor="category-select">Choose a category:</label>
-          <select name="category" id="category-select">
+          <select
+            name="category"
+            id="category-select"
+            value={categories}
+            onChange={handleCategories}
+          >
             <option value="">--Choose a category--</option>
-            <option value="sport">Sport</option>
-            <option value="animals">Animals</option>
-            <option value="food">Food</option>
+            {chooseCategory}
           </select>
           <button onClick={() => props.onClick("startGame")}>Start Game</button>
         </form>
