84503fef64da72a1677bd53943d4763df811c48a
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-02-13 15:18:22 +0100

    add start screen structure and styling

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,3 +1,24 @@
+import "../startScreen.css";
+
 export const StartScreen = (props) => {
-  return <button onClick={() => props.onClick("startGame")}>Start Game</button>;
+  return (
+    <div className="start-screen">
+      <header>
+        <h1>Quiz Game</h1>
+        <p>Description of the game</p>
+      </header>
+      <main>
+        <form>
+          <label htmlFor="category-select">Choose a category:</label>
+          <select name="category" id="category-select">
+            <option value="">--Choose a category--</option>
+            <option value="sport">Sport</option>
+            <option value="animals">Animals</option>
+            <option value="food">Food</option>
+          </select>
+          <button onClick={() => props.onClick("startGame")}>Start Game</button>
+        </form>
+      </main>
+    </div>
+  );
 };
