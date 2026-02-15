6a9f3ec665c28191cd0869ad1a6bd730a12bacba
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-02-15 14:04:32 +0100

    add dummy question object and button for next question

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,6 +1,32 @@
 import style from "../gamePlay.module.css";
 
 export const GamePlay = (props) => {
+  const results = [
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "General Knowledge",
+      question:
+        "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
+      correct_answer: "Jumping over an office chair",
+      incorrect_answers: [
+        "Jumping backwards over a desk",
+        "Standing on his head",
+        "Typing on a keyboard during a handstand",
+      ],
+    },
+    {
+      type: "multiple",
+      difficulty: "medium",
+      category: "General Knowledge",
+      question: "A statue of Charles Darwin sits in what London museum?",
+      correct_answer: "Natural History Museum",
+      incorrect_answers: ["Tate", "British Museum", "Science Museum"],
+    },
+  ];
+
+  console.log(results);
+
   return (
     <div className={style.gamePlay}>
       <header className={style.header}>
@@ -8,15 +34,19 @@ export const GamePlay = (props) => {
         <span className="score">Score: 0</span>
       </header>
       <main>
-        <h2 className={style.question}>
-          What creature had the head of a bull and body of a man?
-        </h2>
+        <h2 className={style.question}>{results[1].question}</h2>
         <button className={style.answer}>Answer 1</button>
         <button className={style.answer}>Answer 2</button>
         <button className={style.answer}>Answer 3</button>
         <button className={style.answer}>Answer 4</button>
       </main>
 
+      {/* If one of the answers is clicked, button is enabled to go to the next question*/}
+      <button className="btn">Next</button>
+
+      {/* update logic
+hide the button and show it only when question.length === 5 && answer selected  */}
+      {/* Navigates to the Score Page */}
       <button onClick={() => props.onClick("showScore")} className="btn">
         Show Score
       </button>
