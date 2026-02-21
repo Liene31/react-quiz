f9ab1583c1de372ba135c154a42411a3beaddf84
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-02-21 12:55:04 +0100

    fix question fetching from API

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -5,7 +5,7 @@ import style from "../gamePlay.module.css";
 
 export const GamePlay = (props) => {
   //State variable
-  // const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
   const [index, setIndex] = useState(0);
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
   const [selectedAnswer, setSelectedAnswer] = useState("");
@@ -13,35 +13,11 @@ export const GamePlay = (props) => {
 
   console.log(props.category);
 
-  const results = [
-    {
-      type: "multiple",
-      difficulty: "medium",
-      category: "General Knowledge",
-      question:
-        "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
-      correct_answer: "Jumping over an office chair",
-      incorrect_answers: [
-        "Jumping backwards over a desk",
-        "Standing on his head",
-        "Typing on a keyboard during a handstand",
-      ],
-    },
-    {
-      type: "multiple",
-      difficulty: "medium",
-      category: "General Knowledge",
-      question: "A statue of Charles Darwin sits in what London museum?",
-      correct_answer: "Natural History Museum",
-      incorrect_answers: ["Tate", "British Museum", "Science Museum"],
-    },
-  ];
-
   useEffect(() => {
     axios
       .get(`https://opentdb.com/api.php?amount=5&category=19&type=multiple`)
       .then((res) => {
-        console.log(res.data.results);
+        setResults(res.data.results);
       })
       .catch((error) => {
         console.log(error);
@@ -58,12 +34,15 @@ export const GamePlay = (props) => {
   };
 
   useEffect(() => {
-    const wrongAnswers = results[index].incorrect_answers;
-    const correctAnswer = results[index].correct_answer;
+    if (!results[index]) {
+      return;
+    }
+    const wrongAnswers = results[index]?.incorrect_answers;
+    const correctAnswer = results[index]?.correct_answer;
     const shuffled = shuffle([...wrongAnswers, correctAnswer]);
 
     setShuffledAnswers(shuffled);
-  }, [index]);
+  }, [index, results]);
 
   //Button - increase the index and moves to next question
   function handleNextQuestion() {
@@ -75,7 +54,7 @@ export const GamePlay = (props) => {
   function handleAnswer(chosenAnswer) {
     setSelectedAnswer(chosenAnswer);
     setScore((prev) => {
-      return results[index].correct_answer === chosenAnswer ? prev + 1 : score;
+      return results[index]?.correct_answer === chosenAnswer ? prev + 1 : prev;
     });
   }
 
@@ -88,7 +67,7 @@ export const GamePlay = (props) => {
         <span className="score">Score: {score}</span>
       </header>
       <main>
-        <h2 className={style.question}>{results[index].question}</h2>
+        <h2 className={style.question}>{results[index]?.question}</h2>
         {shuffledAnswers.map((answers, i) => {
           //User has selected a specific answer but it doesn't evaluate it's correctness
           const isSelected = answers === selectedAnswer;
@@ -115,7 +94,7 @@ export const GamePlay = (props) => {
       </main>
 
       {/* If one of the answers is clicked, button is enabled to go to the next question*/}
-      {index < 1 && (
+      {index < results.length - 1 && (
         <button
           className="btn"
           onClick={handleNextQuestion}
